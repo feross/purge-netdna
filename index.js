cd04a21f725c0a0edd5ce102e24480b86778be33
Feross Aboukhadijeh <feross@feross.org>
2017-04-21 21:57:43 -1000

    standard

--- index.js
+++ index.js
@@ -1,10 +1,10 @@
 var netdna = require('netdna')
 
 module.exports = function (credentials, cb) {
-  if (!credentials.companyAlias) throw(new Error('Missing `companyAlias`!'))
-  if (!credentials.consumerKey) throw(new Error('Missing `consumerKey`!'))
-  if (!credentials.consumerSecret) throw(new Error('Missing `consumerSecret`!'))
-  if (!credentials.zoneId) throw(new Error('Missing `zoneId`!'))
+  if (!credentials.companyAlias) throw new Error('Missing `companyAlias`!')
+  if (!credentials.consumerKey) throw new Error('Missing `consumerKey`!')
+  if (!credentials.consumerSecret) throw new Error('Missing `consumerSecret`!')
+  if (!credentials.zoneId) throw new Error('Missing `zoneId`!')
 
   netdna = netdna({
     companyAlias: credentials.companyAlias,
